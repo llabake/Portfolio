f1a4bab96389e9ff99ff2e10c7e1ec3f69709186
llabake <lemboyelabake@gmail.com>
2019-04-09 13:39:49 +0100

    implemented concern for managing image data

--- app/models/potfolio.rb
+++ app/models/potfolio.rb
@@ -1,4 +1,5 @@
 class Potfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -10,7 +11,7 @@ class Potfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= 'http://placehold.it/600X400'
-    self.thumb_image ||= 'http://placehold.it/350X150'
+    self.main_image ||= Placeholder.image_generator(height: '600', width: "400")
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: "150")
   end
 end
