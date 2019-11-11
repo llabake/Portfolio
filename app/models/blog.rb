2051ba382206a9584253a17285801200a10ce0df
llabake <lemboyelabake@gmail.com>
2019-11-11 13:15:47 +0100

    Implemented redirect for blog shpw page when non admin tries to access draft blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,7 +6,7 @@ class Blog < ApplicationRecord
   friendly_id :title, use: :slugged
   validates_presence_of :title, :body
 
-  # belongs_to :topic
+  belongs_to :topic
 
   has_many :comments, dependent: :destroy
 
