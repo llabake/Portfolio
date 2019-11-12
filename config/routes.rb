30413732e79b93fe16423c4b180f2230d6aa064e
llabake <lemboyelabake@gmail.com>
2019-11-12 13:39:51 +0100

    Built out topic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'topics/index'
+  # get 'topics/show'
+  resources :topics, only: %i[index show]
   # resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :potfolios, except: [:show] do
